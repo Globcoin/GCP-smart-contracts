13c604a20238e09dca6a412bbbba0301f0f66e77
BlockchainPartner <services@blockchainpartner.fr>
2017-11-21 18:56:16 +0100

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# crowdsale-contracts
-Crowdsale contracts
+ # GlobCoin Crypto Platform Sale Contracts
+
+  SmartContracts for the Globcoin Token Sale.
+
+ ## Globcoin Bug Bounty Program
+
+ Thank you for visiting the Globcoin Bug Bounty.
+
+ GLOBCOIN is a digital Coin that marries the best aspects of current cryptocurrencies
+ with the best aspects of fiat currencies. The value of the coin (GLX) is linked to a
+ basket of fiat currencies of the 15 largest world economies and 5 % Gold. Its value
+ reflects the world economy including its new components and trends such as the
+ booming economies of countries like China or India, while maintaining a good
+ balance of the currencies of Western economies. Launched in 2013 by a team based
+ in Switzerland and the UK, the product under the brand GRC “Global reserve
+ Curency” has been already offered to large corporations and high net worth
+ individuals.
+
+
+ Successful bug hunters will be rewarded with up to $5,000 in ether
+ Below you'll find all the details on this program.
+
+ If you&#39;ve already read this information and have found a bug you'd like to submit to Globcoin for review, please use this
+ form:
+
+ ### [Submit a Bug](https://goo.gl/forms/pOEE9WQIM5cKeCTw1).
+
+ ## Rewards
+
+ Paid out Rewards in ether are guided by the Severity category and the Quality of
+ the submission, determined at the sole discretion of Globcoin, and payable at the end
+ of the ICO.
+
+
+ * Critical: Up to $5,000
+ * High: Up to $3,000
+ * Medium: Up to $2,000
+ * Low: Up to $400
+
+ Quality of the submission includes (but not limited to):
+
+ * Quality of Description, Attack Scenario &amp; Components: Clear and well-written
+ descriptions will receive higher rewards.
+
+ * Quality of Reproduction: Include test code, scripts and detailed instructions. The
+ easier it is for us to reproduce and verify the vulnerability, the higher the reward.
+
+ * Quality of Fix: Higher rewards are paid for submissions with clear instructions to
+ fix the vulnerability.
+
+ ## Rules
+ * Issues that have already been submitted by another user or are already
+ known to Globcoin are not eligible for bounty rewards
+
+ * Public disclosure of a vulnerability without Globcoin’s prior consent results in
+ ineligibility for a bounty
+
+ * Determinations of eligibility, award, and all terms related to an award are at
+ the sole and final discretion of Globcoin. Decisions are guided by the
+ submision&#39;s Impact, Likelihood and Quality
+
+
+ ## Running the tests
+
+ These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+ ### Prerequisites
+
+ ```
+ npm install
+
+
+ ```
+
+ ## Running the tests
+
+ ```
+ run ./scripts/test.sh
+
+ ```
---
+++ contracts/GlobCoin.sol
@@ -0,0 +1,247 @@
+pragma solidity ^0.4.11;
+
+import "./crowdsale/CappedCrowdsale.sol";
+import "./crowdsale/RefundableCrowdsale.sol";
+import "./crowdsale/FinalizableCrowdsale.sol";
+import "./token/MintableToken.sol";
+import './math/SafeMath.sol';
+
+
+contract GlobCoinToken is MintableToken {
+  using SafeMath for uint256;
+  string public constant name = "GlobCoin Crypto Platform";
+  string public constant symbol = "GCP";
+  uint8 public constant decimals = 18;
+
+  modifier onlyMintingFinished() {
+    require(mintingFinished == true);
+    _;
+  }
+  /// @dev Same ERC20 behavior, but require the token to be unlocked
+  /// @param _spender address The address which will spend the funds.
+  /// @param _value uint256 The amount of tokens to be spent.
+  function approve(address _spender, uint256 _value) public onlyMintingFinished returns (bool) {
+      return super.approve(_spender, _value);
+  }
+
+  /// @dev Same ERC20 behavior, but require the token to be unlocked
+  /// @param _to address The address to transfer to.
+  /// @param _value uint256 The amount to be transferred.
+  function transfer(address _to, uint256 _value) public onlyMintingFinished returns (bool) {
+      return super.transfer(_to, _value);
+  }
+
+  /// @dev Same ERC20 behavior, but require the token to be unlocked
+  /// @param _from address The address which you want to send tokens from.
+  /// @param _to address The address which you want to transfer to.
+  /// @param _value uint256 the amount of tokens to be transferred.
+  function transferFrom(address _from, address _to, uint256 _value) public onlyMintingFinished returns (bool) {
+    return super.transferFrom(_from, _to, _value);
+  }
+
+}
+
+contract GlobCoinTokenSale is CappedCrowdsale, RefundableCrowdsale {
+
+  // Percentage of token for the Globcoin team.
+  uint8 public constant globFoundationPercentage = 50;
+
+  //Start of the Actual crowdsale. Starblock is the start of the presale.
+  uint256 startSale;
+
+  // Presale Rate per wei ~30% bonus over rate1
+  uint256 public constant PRESALERATE =  170;
+
+  // new rates
+  uint256 public constant RATE1 =  130;
+  uint256 public constant RATE2 =  120;
+  uint256 public constant RATE3 =  110;
+  uint256 public constant RATE4 =  100;
+
+
+  // Cap per tier for bonus in wei.
+  uint256 public constant TIER1 =  10000000000000000000000;
+  uint256 public constant TIER2 =  25000000000000000000000;
+  uint256 public constant TIER3 =  50000000000000000000000;
+
+  //Presale
+  uint256 public weiRaisedPreSale;
+  uint256 public presaleCap;
+
+  function GlobCoinTokenSale(uint256 _startBlock,uint256 _startSale, uint256 _endBlock, uint256 _goal,uint256 _presaleCap, uint256 _cap, address _wallet) CappedCrowdsale(_cap) FinalizableCrowdsale() RefundableCrowdsale(_goal) Crowdsale(_startBlock, _endBlock, _wallet) {
+    require(_goal <= _cap);
+    require(_startSale > _startBlock);
+    require(_endBlock > _startSale);
+    require(_presaleCap > 0);
+    require(_presaleCap < _cap);
+
+    startSale = _startSale;
+    presaleCap = _presaleCap;
+  }
+
+  function createTokenContract() internal returns (MintableToken) {
+    return new GlobCoinToken();
+  }
+
+  //white listed address
+  mapping (address => bool) public whiteListedAddress;
+  mapping (address => bool) public whiteListedAddressPresale;
+
+  modifier onlyPresaleWhitelisted() {
+    require( isWhitelistedPresale(msg.sender) ) ;
+    _;
+  }
+
+  modifier onlyWhitelisted() {
+    require( isWhitelisted(msg.sender) || isWhitelistedPresale(msg.sender) ) ;
+    _;
+  }
+
+  /**
+   * @dev Add a list of address to be whitelisted for the crowdsale only.
+   * @param _users , the list of user Address. Tested for out of gas until 200 addresses.
+   */
+  function whitelistAddresses( address[] _users) onlyOwner {
+    for( uint i = 0 ; i < _users.length ; i++ ) {
+      whiteListedAddress[_users[i]] = true;
+    }
+  }
+
+  function unwhitelistAddress( address _users) onlyOwner {
+    whiteListedAddress[_users] = false;
+  }
+
+  /**
+   * @dev Add a list of address to be whitelisted for the Presale And sale.
+   * @param _users , the list of user Address. Tested for out of gas until 200 addresses.
+   */
+  function whitelistAddressesPresale( address[] _users) onlyOwner {
+    for( uint i = 0 ; i < _users.length ; i++ ) {
+      whiteListedAddressPresale[_users[i]] = true;
+    }
+  }
+
+  function unwhitelistAddressPresale( address _users) onlyOwner {
+    whiteListedAddressPresale[_users] = false;
+  }
+
+  function isWhitelisted(address _user) public constant returns (bool) {
+    return whiteListedAddress[_user];
+  }
+
+  function isWhitelistedPresale(address _user) public constant returns (bool) {
+    return whiteListedAddressPresale[_user];
+  }
+
+  function () payable {
+    if (validPurchasePresale()){
+      buyTokensPresale(msg.sender);
+    } else {
+      buyTokens(msg.sender);
+    }
+  }
+
+  function buyTokens(address beneficiary) payable onlyWhitelisted {
+    require(beneficiary != 0x0);
+    require(validPurchase());
+
+    uint256 weiAmount = msg.value;
+    uint256 tokens = calculateTokenAmount(weiAmount);
+    weiRaised = weiRaised.add(weiAmount);
+
+    token.mint(beneficiary, tokens);
+    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+    forwardFunds();
+  }
+
+  function buyTokensPresale(address beneficiary) payable onlyPresaleWhitelisted {
+    require(beneficiary != 0x0);
+    require(validPurchasePresale());
+
+    uint256 weiAmount = msg.value;
+    uint256 tokens = weiAmount.mul(PRESALERATE);
+    weiRaisedPreSale = weiRaisedPreSale.add(weiAmount);
+
+    token.mint(beneficiary, tokens);
+    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+    forwardFunds();
+  }
+
+  // calculate the amount of token the user is getting - can overlap on multiple tiers.
+  function calculateTokenAmount(uint256 weiAmount) internal returns (uint256){
+    uint256 amountToBuy = weiAmount;
+    uint256 amountTokenBought;
+    uint256 currentWeiRaised = weiRaised;
+     if (currentWeiRaised < TIER1 && amountToBuy > 0) {
+       var (amountBoughtInTier, amountLeftTobuy) = calculateAmountPerTier(amountToBuy,TIER1,RATE1,currentWeiRaised);
+       amountTokenBought = amountTokenBought.add(amountBoughtInTier);
+       currentWeiRaised = currentWeiRaised.add(amountToBuy.sub(amountLeftTobuy));
+       amountToBuy = amountLeftTobuy;
+     }
+     if (currentWeiRaised < TIER2 && amountToBuy > 0) {
+      (amountBoughtInTier, amountLeftTobuy) = calculateAmountPerTier(amountToBuy,TIER2,RATE2,currentWeiRaised);
+      amountTokenBought = amountTokenBought.add(amountBoughtInTier);
+      currentWeiRaised = currentWeiRaised.add(amountToBuy.sub(amountLeftTobuy));
+      amountToBuy = amountLeftTobuy;
+     }
+     if (currentWeiRaised < TIER3 && amountToBuy > 0) {
+      (amountBoughtInTier, amountLeftTobuy) = calculateAmountPerTier(amountToBuy,TIER3,RATE3,currentWeiRaised);
+      amountTokenBought = amountTokenBought.add(amountBoughtInTier);
+      currentWeiRaised = currentWeiRaised.add(amountToBuy.sub(amountLeftTobuy));
+      amountToBuy = amountLeftTobuy;
+     }
+    if ( currentWeiRaised < cap && amountToBuy > 0) {
+      (amountBoughtInTier, amountLeftTobuy) = calculateAmountPerTier(amountToBuy,cap,RATE4,currentWeiRaised);
+      amountTokenBought = amountTokenBought.add(amountBoughtInTier);
+      currentWeiRaised = currentWeiRaised.add(amountToBuy.sub(amountLeftTobuy));
+      amountToBuy = amountLeftTobuy;
+    }
+    return amountTokenBought;
+  }
+
+  // calculate the amount of token within a tier.
+  function calculateAmountPerTier(uint256 amountToBuy,uint256 tier,uint256 rate,uint256 currentWeiRaised) internal returns (uint256,uint256) {
+    uint256 amountAvailable = tier.sub(currentWeiRaised);
+    if ( amountToBuy > amountAvailable ) {
+      uint256 amountBoughtInTier = amountAvailable.mul(rate);
+      amountToBuy = amountToBuy.sub(amountAvailable);
+      return (amountBoughtInTier,amountToBuy);
+    } else {
+      amountBoughtInTier = amountToBuy.mul(rate);
+      return (amountBoughtInTier,0);
+    }
+  }
+
+  function finalization() internal {
+    if (goalReached()) {
+      //Globcoin gets 100% of the amount of tokens created through the crowdsale. (50% of the total token)
+      uint256 totalSupply = token.totalSupply();
+      token.mint(wallet, totalSupply);
+      token.finishMinting();
+    }
+    super.finalization();
+  }
+
+  // Override of the validPurchase function so that the new sale periode start at StartSale instead of Startblock.
+  function validPurchase() internal constant returns (bool) {
+    bool withinPeriod = block.number >= startSale && block.number <= endBlock;
+    bool nonZeroPurchase = msg.value != 0;
+    bool withinCap = weiRaised.add(msg.value) <= cap;
+    return withinCap && withinPeriod && nonZeroPurchase;
+  }
+
+  // Sale period start at StartBlock until the sale Start ( startSale )
+  function validPurchasePresale() internal constant returns (bool) {
+    bool withinPeriod = block.number >= startBlock && block.number < startSale;
+    bool nonZeroPurchase = msg.value != 0;
+    bool withinCap = weiRaisedPreSale.add(msg.value) <= presaleCap;
+    return withinPeriod && nonZeroPurchase && withinCap;
+  }
+
+  // Override of the goalReached function so that the goal take into account the token raised during the Presale.
+  function goalReached() public constant returns (bool) {
+    uint256 totalWeiRaised = weiRaisedPreSale.add(weiRaised);
+    return totalWeiRaised >= goal || super.goalReached();
+  }
+
+}
