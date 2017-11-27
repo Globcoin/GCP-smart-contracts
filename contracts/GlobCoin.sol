2572770e4bd30f04cf7d974e0ad6f6e21f4abcff
BlockchainPartner <services@blockchainpartner.fr>
2017-11-27 12:30:38 +0100

    removed token and unused var

--- contracts/GlobCoin.sol
+++ contracts/GlobCoin.sol
@@ -2,49 +2,10 @@ pragma solidity ^0.4.11;
 
 import "./crowdsale/CappedCrowdsale.sol";
 import "./crowdsale/RefundableCrowdsale.sol";
-import "./crowdsale/FinalizableCrowdsale.sol";
-import "./token/MintableToken.sol";
 import './math/SafeMath.sol';
-
-
-contract GlobCoinToken is MintableToken {
-  using SafeMath for uint256;
-  string public constant name = "GlobCoin Crypto Platform";
-  string public constant symbol = "GCP";
-  uint8 public constant decimals = 18;
-
-  modifier onlyMintingFinished() {
-    require(mintingFinished == true);
-    _;
-  }
-  /// @dev Same ERC20 behavior, but require the token to be unlocked
-  /// @param _spender address The address which will spend the funds.
-  /// @param _value uint256 The amount of tokens to be spent.
-  function approve(address _spender, uint256 _value) public onlyMintingFinished returns (bool) {
-      return super.approve(_spender, _value);
-  }
-
-  /// @dev Same ERC20 behavior, but require the token to be unlocked
-  /// @param _to address The address to transfer to.
-  /// @param _value uint256 The amount to be transferred.
-  function transfer(address _to, uint256 _value) public onlyMintingFinished returns (bool) {
-      return super.transfer(_to, _value);
-  }
-
-  /// @dev Same ERC20 behavior, but require the token to be unlocked
-  /// @param _from address The address which you want to send tokens from.
-  /// @param _to address The address which you want to transfer to.
-  /// @param _value uint256 the amount of tokens to be transferred.
-  function transferFrom(address _from, address _to, uint256 _value) public onlyMintingFinished returns (bool) {
-    return super.transferFrom(_from, _to, _value);
-  }
-
-}
+import './GlobcoinToken.sol';
 
 contract GlobCoinTokenSale is CappedCrowdsale, RefundableCrowdsale {
-
-  // Percentage of token for the Globcoin team.
-  uint8 public constant globFoundationPercentage = 50;
 
   //Start of the Actual crowdsale. Starblock is the start of the presale.
   uint256 startSale;
